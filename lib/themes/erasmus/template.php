0bd36c6144610364fcee1c47070de27eaba63d32
MehdiDonk <mehdi.naqi@gmail.com>
2016-10-03 10:46:24 +0200

    Update template.php

--- lib/themes/erasmus/template.php
+++ lib/themes/erasmus/template.php
@@ -156,144 +156,144 @@ function erasmus_preprocess_om_maximenu_submenu(&$variables) {
  * Implements template_preprocess_page().
  */
 function erasmus_preprocess_page(&$variables, $hook) {
-$header = drupal_get_http_header("status");
-
-if ($header == "404 Not Found") {
-  $variables['theme_hook_suggestions'][] = 'page__404';
-}
-global $base_url;
-$variables['base_url'] = $base_url;
-global $language;
-$variables['is_newlayoutr'] = FALSE;
-$variables['language'] = $language;
-$variables['abouttitle'] = theme_get_setting('abouttitle');
-$variables['aboutsubtitle'] = theme_get_setting('aboutsubtitle');
-$variables['individualstitle'] = theme_get_setting('individualstitle');
-$variables['individualssubtitle'] = theme_get_setting('individualssubtitle');
-$variables['organisationstitle'] = theme_get_setting('organisationstitle');
-$variables['resourcestitle'] = theme_get_setting('resourcestitle');
-$variables['resourcessubtitle'] = theme_get_setting('resourcessubtitle');
-$variables['documentlibrarytitle'] = theme_get_setting('documentlibrarytitle');
-$variables['documentlibrarysubtitle'] = theme_get_setting('documentlibrarysubtitle');
-$variables['contacttitle'] = theme_get_setting('contacttitle');
-$variables['contactsubtitle'] = theme_get_setting('organisationssubtitle');
-$variables['submenu'] = t('Opportunities');
-$variables['submenuabout'] = t('About');
-$variables['submenuresources'] = t('Resources');
-$variables['contactsub'] = t('Contact points');
-$variables['modalnewsletter'] = l(
-        t('Subscribe to our newsletter'),
-        "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
-        action=index&serviceid=1756&pk_campaign=SubscribeNewsletter&pk_kwd=Modal",
-        array(
-          'attributes' => array(
-            'class' => 'link-modal',
-            'align' => 'center',
-          ),
-        )
-      );
-$variables['updatestitle'] = t('Updates');
-$variables['allevents'] = l(
-        t('All events'),
-        'all-events',
-        array('attributes' => array('class' => array('link-more-white-blue')))
-    );
-$variables['allcalls'] = l(
-        t('All calls'), 'calls-for-proposals-tenders',
-        array(
-          'attributes' => array('class' => array('link-more-white-blue')),
-        )
-    );
-$variables['allnews'] = l(
-        t('All news'), 'all-news',
-        array(
-          'attributes' => array(
-            'class' => array(
-              'link-more-white-blue',
-              'another-class',
+  $header = drupal_get_http_header("status");
+  
+  if ($header == "404 Not Found") {
+    $variables['theme_hook_suggestions'][] = 'page__404';
+  }
+  global $base_url;
+  $variables['base_url'] = $base_url;
+  global $language;
+  $variables['is_newlayoutr'] = FALSE;
+  $variables['language'] = $language;
+  $variables['abouttitle'] = theme_get_setting('abouttitle');
+  $variables['aboutsubtitle'] = theme_get_setting('aboutsubtitle');
+  $variables['individualstitle'] = theme_get_setting('individualstitle');
+  $variables['individualssubtitle'] = theme_get_setting('individualssubtitle');
+  $variables['organisationstitle'] = theme_get_setting('organisationstitle');
+  $variables['resourcestitle'] = theme_get_setting('resourcestitle');
+  $variables['resourcessubtitle'] = theme_get_setting('resourcessubtitle');
+  $variables['documentlibrarytitle'] = theme_get_setting('documentlibrarytitle');
+  $variables['documentlibrarysubtitle'] = theme_get_setting('documentlibrarysubtitle');
+  $variables['contacttitle'] = theme_get_setting('contacttitle');
+  $variables['contactsubtitle'] = theme_get_setting('organisationssubtitle');
+  $variables['submenu'] = t('Opportunities');
+  $variables['submenuabout'] = t('About');
+  $variables['submenuresources'] = t('Resources');
+  $variables['contactsub'] = t('Contact points');
+  $variables['modalnewsletter'] = l(
+          t('Subscribe to our newsletter'),
+          "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
+          action=index&serviceid=1756&pk_campaign=SubscribeNewsletter&pk_kwd=Modal",
+          array(
+            'attributes' => array(
+              'class' => 'link-modal',
+              'align' => 'center',
             ),
-          ),
-        )
-    );
-if (isset($variables['node'])) {
-  $node = $variables['node'];
-  if ($node->type == 'opportunities_for_individuals'
-      || $node->type == 'document_library'
-      || $node->type == 'opportunities_for_organisations'
-      || $node->type == 'resources'
-  ) {
-    $variables['colheightortwelve'] = 'col-lg-8';
+          )
+        );
+  $variables['updatestitle'] = t('Updates');
+  $variables['allevents'] = l(
+          t('All events'),
+          'all-events',
+          array('attributes' => array('class' => array('link-more-white-blue')))
+      );
+  $variables['allcalls'] = l(
+          t('All calls'), 'calls-for-proposals-tenders',
+          array(
+            'attributes' => array('class' => array('link-more-white-blue')),
+          )
+      );
+  $variables['allnews'] = l(
+          t('All news'), 'all-news',
+          array(
+            'attributes' => array(
+              'class' => array(
+                'link-more-white-blue',
+                'another-class',
+              ),
+            ),
+          )
+      );
+  if (isset($variables['node'])) {
+    $node = $variables['node'];
+    if ($node->type == 'opportunities_for_individuals'
+        || $node->type == 'document_library'
+        || $node->type == 'opportunities_for_organisations'
+        || $node->type == 'resources'
+    ) {
+      $variables['colheightortwelve'] = 'col-lg-8';
+    }
+    else {
+      $variables['colheightortwelve'] = 'col-lg-12';
+    }
+    $variables['is_newlayoutr'] = $node->type == 'opportunities_for_individuals'
+              || $node->type == 'document_library'
+              || $node->type == 'opportunities_for_organisations'
+              || $node->type == 'resources';
+    if ($node->type == 'opportunities_for_individuals') {
+      $variables['bgpage'] = 'individualsbg';
+    }
+    elseif ($node->type == 'opportunities_for_organisations') {
+      $variables['bgpage'] = 'organisationsbg';
+    }
+    elseif ($node->type == 'resources') {
+      $variables['bgpage'] = 'resources';
+    }
+    else {
+      $variables['bgpage'] = 'frontbg';
+    }
   }
   else {
     $variables['colheightortwelve'] = 'col-lg-12';
-  }
-  $variables['is_newlayoutr'] = $node->type == 'opportunities_for_individuals'
-            || $node->type == 'document_library'
-            || $node->type == 'opportunities_for_organisations'
-            || $node->type == 'resources';
-  if ($node->type == 'opportunities_for_individuals') {
-    $variables['bgpage'] = 'individualsbg';
-  }
-  elseif ($node->type == 'opportunities_for_organisations') {
-    $variables['bgpage'] = 'organisationsbg';
-  }
-  elseif ($node->type == 'resources') {
-    $variables['bgpage'] = 'resources';
-  }
-  else {
     $variables['bgpage'] = 'frontbg';
   }
-}
-else {
-  $variables['colheightortwelve'] = 'col-lg-12';
-  $variables['bgpage'] = 'frontbg';
-}
-
-/* Add page tpl based on content type */
-
-if (isset($node) && $node->type == 'programme_guide') {
-  $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
-
-  drupal_add_css(
-    path_to_theme() . '/css/eac_erasmus_guide.css',
-    'theme', 'all', TRUE
-  );
-
-  drupal_add_js(
-    path_to_theme() . '/scripts/erasmus_guide.js'
-  );
-
-}
-else {
-  drupal_add_css(
-    path_to_theme() . '/css/erasmus.css',
-     array('group' => CSS_SYSTEM + 9999, 'preprocess' => FALSE),
-    'theme', 'all', TRUE
-  );
-
-}
-/**
- * Implements template_preprocess_node().
- */
-function erasmus_preprocess_node(&$variables) {
-  $node = $variables['node'];
-  if ($node->type == 'video_gallery') {
-    $content = $variables['content'];
-    if (isset($content['field_embed_code'])) {
-      $variables['embed_code'] = $content['field_embed_code'][0]['#markup'];
-    }
-
-    if ($content['title_field']) {
-      $variables['article_title'] = $content['title_field'][0]['#markup'];
-    }
-
-    if (isset($content['field_abstract'])) {
-      $variables['abstract'] = $content['field_abstract'][0]['#markup'];
-    }
-
-    if ($content['body']) {
-      $variables['body'] = $content['body'][0]['#markup'];
+  
+  /* Add page tpl based on content type */
+  
+  if (isset($node) && $node->type == 'programme_guide') {
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  
+    drupal_add_css(
+      path_to_theme() . '/css/eac_erasmus_guide.css',
+      'theme', 'all', TRUE
+    );
+  
+    drupal_add_js(
+      path_to_theme() . '/scripts/erasmus_guide.js'
+    );
+  
+  }
+  else {
+    drupal_add_css(
+      path_to_theme() . '/css/erasmus.css',
+       array('group' => CSS_SYSTEM + 9999, 'preprocess' => FALSE),
+      'theme', 'all', TRUE
+    );
+  
+  }
+  /**
+   * Implements template_preprocess_node().
+   */
+  function erasmus_preprocess_node(&$variables) {
+    $node = $variables['node'];
+    if ($node->type == 'video_gallery') {
+      $content = $variables['content'];
+      if (isset($content['field_embed_code'])) {
+        $variables['embed_code'] = $content['field_embed_code'][0]['#markup'];
+      }
+  
+      if ($content['title_field']) {
+        $variables['article_title'] = $content['title_field'][0]['#markup'];
+      }
+  
+      if (isset($content['field_abstract'])) {
+        $variables['abstract'] = $content['field_abstract'][0]['#markup'];
+      }
+  
+      if ($content['body']) {
+        $variables['body'] = $content['body'][0]['#markup'];
+      }
     }
   }
-}
 }
