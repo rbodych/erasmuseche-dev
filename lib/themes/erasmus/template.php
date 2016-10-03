7167749b85e8d9f92c3f48f77b89186dd714fd02
MehdiDonk <mehdi.naqi@gmail.com>
2016-10-03 15:17:56 +0200

    Update template.php

--- lib/themes/erasmus/template.php
+++ lib/themes/erasmus/template.php
@@ -27,10 +27,10 @@ function erasmus_preprocess_html(&$variables) {
   $settings['erasmus']['videohome_youtube_id'] = theme_get_setting('videohome_youtube_id');
   drupal_add_js($settings, 'setting');
   drupal_add_js(
-        drupal_get_path('theme', 'erasmus') . '/scripts/videohome.js', array(
-          'scope' => 'footer',
-        )
-    );
+    drupal_get_path('theme', 'erasmus') . '/scripts/videohome.js', array(
+      'scope' => 'footer',
+    )
+  );
 }
 
 /**
@@ -44,112 +44,112 @@ function erasmus_preprocess_om_maximenu_submenu(&$variables) {
   $variables['language'] = $language;
   $variables['namesite'] = t('Erasmus+');
   $variables['facebook'] = l(
-        $empty, "https://www.facebook.com/EUErasmusPlusProgramme",
-        array('attributes' => array('class' => 'icon facebook'))
-    );
+    $empty, "https://www.facebook.com/EUErasmusPlusProgramme",
+    array('attributes' => array('class' => 'icon facebook'))
+  );
   $variables['twitter'] = l(
-        $empty, "https://twitter.com/EUErasmusPlus",
-        array('attributes' => array('class' => 'icon twitter'))
-    );
+    $empty, "https://twitter.com/EUErasmusPlus",
+    array('attributes' => array('class' => 'icon twitter'))
+  );
   $variables['gplus'] = l(
-        $empty, "https://plus.google.com/communities/115077875344973792963",
-        array('attributes' => array('class' => 'icon gplus'))
-    );
+    $empty, "https://plus.google.com/communities/115077875344973792963",
+    array('attributes' => array('class' => 'icon gplus'))
+  );
   $variables['iconnewsletter'] = l(
-        $empty,
-        "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
-        action=index&serviceid=1756&pk_campaign=NewErasmus&pk_kwd=NewsletterDesktop",
-        array(
-          'attributes' => array(
-            'class' => 'fa fa-newspaper-o',
-          ),
-        )
-    );
+    $empty,
+    "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
+    action=index&serviceid=1756&pk_campaign=NewErasmus&pk_kwd=NewsletterDesktop",
+    array(
+        'attributes' => array(
+        'class' => 'fa fa-newspaper-o',
+      ),
+    )
+  );
   $variables['newsletterdesktop'] = l(
-        t('Newsletter'),
-        "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
-        action=index&serviceid=1756&pk_campaign=NewErasmus&pk_kwd=NewsletterDesktop",
-        array(
-          'attributes' => array(
-            'class' => 'newsletter',
-          ),
-        )
-    );
+    t('Newsletter'),
+    "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
+    action=index&serviceid=1756&pk_campaign=NewErasmus&pk_kwd=NewsletterDesktop",
+    array(
+      'attributes' => array(
+        'class' => 'newsletter',
+      ),
+    )
+  );
   $variables['newslettermobile'] = l(
-        t('Newsletter'),
-        "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
-        action=index&serviceid=1756&pk_campaign=NewErasmus&pk_kwd=NewsletterDesktop",
-        array(
-          'attributes' => array(
-            'class' => 'nav-tool-expand',
-            'data-expand' => 'mobile-nav-newsletter',
-          ),
-        )
-    );
+    t('Newsletter'),
+    "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
+    action=index&serviceid=1756&pk_campaign=NewErasmus&pk_kwd=NewsletterDesktop",
+    array(
+      'attributes' => array(
+        'class' => 'nav-tool-expand',
+        'data-expand' => 'mobile-nav-newsletter',
+      ),
+    )
+  );
   $variables['searchlink'] = l(
-        t('Search'), $base_url . "/search/site",
-        array(
-          'attributes' => array(
-            'class' => 'nav-tool-expand',
-            'data-expand' => 'mobile-nav-newsletter',
-          ),
-        )
-    );
+    t('Search'), $base_url . "/search/site",
+    array(
+      'attributes' => array(
+        'class' => 'nav-tool-expand',
+        'data-expand' => 'mobile-nav-newsletter',
+      ),
+    )
+  );
   $variables['lang'] = l(
-        t('English'), '',
-        array(
-          'fragment' => 'tablang',
-          'external' => TRUE,
-          'attributes' => array(
-            'class' => 'nav-tool-expand',
-            'data-expand' => 'mobile-nav-lg',
-          ),
-        )
-    );
+    t('English'), '',
+    array(
+      'fragment' => 'tablang',
+      'external' => TRUE,
+      'attributes' => array(
+        'class' => 'nav-tool-expand',
+        'data-expand' => 'mobile-nav-lg',
+      ),
+    )
+  );
   $variables['langintro'] = t(
-        'The site is currently only available in English, 
-        other languages will be available later.'
-    );
+    'The site is currently only available in English, 
+    other languages will be available later.'
+  );
   $variables['langlink'] = l(
-        t('The previous version'), "http://ec.europa.eu/programmes
-        /erasmus-plus/index_en.htm&pk_campaign=NewErasmus&pk_kwd=LinkToOldVersion",
-        array('attributes' => array('class' => 'linklanguage'))
-    );
+    t('The previous version'), "http://ec.europa.eu/programmes
+    /erasmus-plus/index_en.htm&pk_campaign=NewErasmus&pk_kwd=LinkToOldVersion",
+    array('attributes' => array('class' => 'linklanguage'))
+  );
   $variables['langsummary'] = t(
-        '(available in all languages) has been archived 
-        and will be taken offline on 01/01/2016.'
-    );
+    '(available in all languages) has been archived 
+    and will be taken offline on 01/01/2016.'
+  );
   $variables['eventlink'] = l(
-        t('Events'), 'all-events',
-        array('attributes' => array('class' => array('linklanguage')))
-    );
+    t('Events'), 'all-events',
+    array('attributes' => array('class' => array('linklanguage')))
+  );
   $variables['callslink'] = l(
-        t('Calls'), 'calls-for-proposals-tenders',
-        array('attributes' => array('class' => array('linklanguage')))
-    );
+    t('Calls'), 'calls-for-proposals-tenders',
+    array('attributes' => array('class' => array('linklanguage')))
+  );
   $variables['newslink'] = l(
-        t('News'), 'all-news', array(
-          'attributes' => array(
-            'class' => array('linklanguage'),
-          ),
-        )
-    );
+    t('News'), 'all-news', array(
+      'attributes' => array(
+        'class' => array('linklanguage'),
+      ),
+    )
+  );
   $variables['nav_ico'] = theme(
     'image', array(
       'path' => $base_url . '/sites/all/themes/erasmus' . '/images/mobile-nav-ico.svg',
     )
-   );
+  );
   $variables['flat_ec_logo'] = theme(
-        'image', array(
-          'path' => $base_url . '/sites/all/themes/erasmus'
-          . '/images/mobile-flat-ec-logo.svg',
-        )
-    );
+    'image', array(
+      'path' => $base_url . '/sites/all/themes/erasmus'
+      . '/images/mobile-flat-ec-logo.svg',
+    )
+  );
   $variables['logo_ce_en'] = theme(
-        'image', array(
-          'path' => $base_url . '/sites/all/themes/erasmus' . '/images/logo_ce-en.svg',
-        )
-    );
+    'image', array(
+      'path' => $base_url . '/sites/all/themes/erasmus' . '/images/logo_ce-en.svg',
+    )
+  );
 }
 
 /**
@@ -157,7 +157,6 @@ function erasmus_preprocess_om_maximenu_submenu(&$variables) {
  */
 function erasmus_preprocess_page(&$variables, $hook) {
   $header = drupal_get_http_header("status");
-  
   if ($header == "404 Not Found") {
     $variables['theme_hook_suggestions'][] = 'page__404';
   }
@@ -182,45 +181,45 @@ function erasmus_preprocess_page(&$variables, $hook) {
   $variables['submenuresources'] = t('Resources');
   $variables['contactsub'] = t('Contact points');
   $variables['modalnewsletter'] = l(
-          t('Subscribe to our newsletter'),
-          "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
-          action=index&serviceid=1756&pk_campaign=SubscribeNewsletter&pk_kwd=Modal",
-          array(
-            'attributes' => array(
-              'class' => 'link-modal',
-              'align' => 'center',
-            ),
-          )
-        );
+    t('Subscribe to our newsletter'),
+    "https://ec.europa.eu/coreservices/mailing/index.cfm?controller=register&
+    action=index&serviceid=1756&pk_campaign=SubscribeNewsletter&pk_kwd=Modal",
+    array(
+      'attributes' => array(
+        'class' => 'link-modal',
+        'align' => 'center',
+      ),
+    )
+  );
   $variables['updatestitle'] = t('Updates');
   $variables['allevents'] = l(
-          t('All events'),
-          'all-events',
-          array('attributes' => array('class' => array('link-more-white-blue')))
-      );
+    t('All events'),
+    'all-events',
+    array('attributes' => array('class' => array('link-more-white-blue')))
+  );
   $variables['allcalls'] = l(
-          t('All calls'), 'calls-for-proposals-tenders',
-          array(
-            'attributes' => array('class' => array('link-more-white-blue')),
-          )
-      );
+    t('All calls'), 'calls-for-proposals-tenders',
+    array(
+      'attributes' => array('class' => array('link-more-white-blue')),
+    )
+  );
   $variables['allnews'] = l(
-          t('All news'), 'all-news',
-          array(
-            'attributes' => array(
-              'class' => array(
-                'link-more-white-blue',
-                'another-class',
-              ),
-            ),
-          )
-      );
+    t('All news'), 'all-news',
+    array(
+      'attributes' => array(
+        'class' => array(
+          'link-more-white-blue',
+          'another-class',
+        ),
+      ),
+    )
+  );
   if (isset($variables['node'])) {
     $node = $variables['node'];
     if ($node->type == 'opportunities_for_individuals'
-        || $node->type == 'document_library'
-        || $node->type == 'opportunities_for_organisations'
-        || $node->type == 'resources'
+      || $node->type == 'document_library'
+      || $node->type == 'opportunities_for_organisations'
+      || $node->type == 'resources'
     ) {
       $variables['colheightortwelve'] = 'col-lg-8';
     }
@@ -228,9 +227,9 @@ function erasmus_preprocess_page(&$variables, $hook) {
       $variables['colheightortwelve'] = 'col-lg-12';
     }
     $variables['is_newlayoutr'] = $node->type == 'opportunities_for_individuals'
-              || $node->type == 'document_library'
-              || $node->type == 'opportunities_for_organisations'
-              || $node->type == 'resources';
+      || $node->type == 'document_library'
+      || $node->type == 'opportunities_for_organisations'
+      || $node->type == 'resources';
     if ($node->type == 'opportunities_for_individuals') {
       $variables['bgpage'] = 'individualsbg';
     }
@@ -248,21 +247,17 @@ function erasmus_preprocess_page(&$variables, $hook) {
     $variables['colheightortwelve'] = 'col-lg-12';
     $variables['bgpage'] = 'frontbg';
   }
-  
   /* Add page tpl based on content type */
-  
   if (isset($node) && $node->type == 'programme_guide') {
     $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
-  
     drupal_add_css(
       path_to_theme() . '/css/eac_erasmus_guide.css',
       'theme', 'all', TRUE
     );
-  
+
     drupal_add_js(
       path_to_theme() . '/scripts/erasmus_guide.js'
     );
-  
   }
   else {
     drupal_add_css(
@@ -270,8 +265,9 @@ function erasmus_preprocess_page(&$variables, $hook) {
        array('group' => CSS_SYSTEM + 9999, 'preprocess' => FALSE),
       'theme', 'all', TRUE
     );
-  
+
   }
+
   /**
    * Implements template_preprocess_node().
    */
@@ -282,18 +278,19 @@ function erasmus_preprocess_page(&$variables, $hook) {
       if (isset($content['field_embed_code'])) {
         $variables['embed_code'] = $content['field_embed_code'][0]['#markup'];
       }
-  
+
       if ($content['title_field']) {
         $variables['article_title'] = $content['title_field'][0]['#markup'];
       }
-  
+
       if (isset($content['field_abstract'])) {
         $variables['abstract'] = $content['field_abstract'][0]['#markup'];
       }
-  
+
       if ($content['body']) {
         $variables['body'] = $content['body'][0]['#markup'];
       }
     }
   }
+
 }
