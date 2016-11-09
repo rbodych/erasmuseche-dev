151801e093c1f4847f01350824907001bca653be
cwris <chris.maertens@gmail.com>
2016-11-09 18:21:16 +0000

    MULTISITE-14042 - fixes

--- lib/themes/erasmus/template.php
+++ lib/themes/erasmus/template.php
@@ -35,7 +35,6 @@ function erasmus_preprocess_html(&$variables) {
 
 /**
  * Process variables for om_maximenu_submenu.tpl.php
- *
  */
 function erasmus_preprocess_om_maximenu_submenu(&$variables) {
   global $base_url;
