cd8f08213459850a93ff1ec27d077a4a4763018b
michael schuddings <michael.schuddings@one-agency.be>
2016-12-20 13:23:33 +0000

    OVerride theming functions for select list, this is used for exposed view countries filter.

--- lib/themes/erasmus_30year_anniversary/template.php
+++ lib/themes/erasmus_30year_anniversary/template.php
@@ -19,3 +19,51 @@
  *
  * Ec_resp's theme implementation to display a single Drupal page.
  */
+
+/**
+ * Implements theme_select().
+ */
+function erasmus_30year_anniversary_select($variables) {
+  $element = $variables['element'];
+  element_set_attributes($element, array('id', 'name', 'size'));
+  _form_set_class($element, array('form-select'));
+
+  return '<select' . drupal_attributes($element['#attributes']) . '>' . erasmus_30year_anniversary_form_select_options($element) . '</select>';
+}
+
+/**
+ * Implements form_select_options().
+ */
+function erasmus_30year_anniversary_form_select_options($element, $choices = NULL) {
+  if (!isset($choices)) {
+    $choices = $element['#options'];
+  }
+  // array_key_exists() accommodates the rare event where $element['#value'] is NULL.
+  // isset() fails in this situation.
+  $value_valid = isset($element['#value']) || array_key_exists('#value', $element);
+  $value_is_array = $value_valid && is_array($element['#value']);
+  $options = '';
+  foreach ($choices as $key => $choice) {
+    if (is_array($choice)) {
+      $options .= '<optgroup label="' . check_plain($key) . '">';
+      $options .= form_select_options($element, $choice);
+      $options .= '</optgroup>';
+    }
+    elseif (is_object($choice)) {
+      $options .= form_select_options($element, $choice->option);
+    }
+    else {
+      $key = (string) $key;
+      if ($value_valid && (!$value_is_array && (string) $element['#value'] === $key || ($value_is_array && in_array($key, $element['#value'])))) {
+        $selected = ' selected="selected"';
+      }
+      else {
+        $selected = '';
+      }
+      $options .= '<option class="' . drupal_clean_css_identifier($choice) .
+        '" value="' . check_plain($key) . '"' . $selected . '>'
+        . check_plain($choice) . '</option>';
+    }
+  }
+  return $options;
+}
