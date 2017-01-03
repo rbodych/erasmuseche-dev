a6732a13ebf9b8651be4b7b4adc6b9435e97c6be
michael schuddings <michael.schuddings@one-agency.be>
2017-01-03 12:32:04 +0000

    Card cleanup TPL.

--- lib/themes/erasmus_30year_anniversary/templates/node/node--card.tpl.php
+++ lib/themes/erasmus_30year_anniversary/templates/node/node--card.tpl.php
@@ -4,21 +4,68 @@
  * Default theme implementation to display a node.
  */
 ?>
-<div class="node--card clearfix"<?php print $attributes; ?>>
+
+<article class="node--card h-event">
 
   <?php print render($title_prefix); ?>
   <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+    <h3><?php print $title; ?></h3>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+  ?>
+  <?php if (isset($image)): ?>
+    <img src="<?php print $image; ?>" alt="<?php print $title; ?>" />
+  <?php endif; ?>
+  <?php if (isset($date)): ?>
+    <p class="dt-start">
+      <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+      <?php print $date; ?>
+    </p>
+  <?php endif; ?>
+  <?php if (isset($city)): ?>
+    <p class="p-location">
+      <span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+      <?php print $city . ', ' . $country; ?>
+    </p>
+  <?php endif; ?>
+  <?php if (isset($sector) || isset($theme)): ?>
+    <p class="p-category">
+      <span class="glyphicon glyphicon-tag" aria-hidden="true"></span>
+      <?php print isset($sector) ? $sector: ''; ?>
+      <?php print isset($theme) ? $theme: ''; ?>
+    </p>
+  <?php endif; ?>
+  <?php if (isset($intro)): ?>
+    <p class="p-summary">
+      <?php print $intro; ?>
+    </p>
+  <?php endif; ?>
 
-</div>
+  <?php if (isset($expected_participants)): ?>
+    <div class="node--card__participants">
+      <p><?php print $expected_participants['number']; ?></p>
+      <h4><?php print $expected_participants['title']; ?></h4>
+    </div>
+  <?php endif; ?>
+  <ul class="node--card__links">
+    <?php if (isset($link)): ?>
+      <li><a class="button button--primary button--medium" href="<?php print $link['url']; ?>" alt="<?php print $link['title']; ?>">
+        <?php print $link['title']; ?>
+        </a>
+      </li>
+    <?php endif; ?>
+    <?php if (isset($organiser)): ?>
+      <li><a class="button button--stroke-primary button--medium" href="<?php print $organiser['url']; ?>"
+        alt="<?php print $organiser['title']; ?>">
+        <?php print $organiser['title']; ?>
+        </a>
+      </li>
+    <?php endif; ?>
+  </ul>
+
+</article>
