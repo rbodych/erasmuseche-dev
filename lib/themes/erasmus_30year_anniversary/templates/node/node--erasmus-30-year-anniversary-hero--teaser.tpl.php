0ea9bfd7bf4308eaf2d27cc059d12d20e06f171b
michael schuddings <michael.schuddings@one-agency.be>
2017-01-04 11:41:16 +0000

    Background on hero teaser to separate div.

--- lib/themes/erasmus_30year_anniversary/templates/node/node--erasmus-30-year-anniversary-hero--teaser.tpl.php
+++ lib/themes/erasmus_30year_anniversary/templates/node/node--erasmus-30-year-anniversary-hero--teaser.tpl.php
@@ -4,9 +4,10 @@
  * Default theme implementation to display a node.
  */
 ?>
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>
-  style="background: url('<?php print render($content['banner_url']); ?>')">
-
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  
+  <div class="background" data-url="<?php print render($content['banner_url']); ?>"></div>
+  
   <?php print render($title_prefix); ?>
   <?php if (!$page): ?>
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
