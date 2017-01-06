06fe750a7d79485debdfdd37c83c9170e74eea19
michael schuddings <michael.schuddings@one-agency.be>
2017-01-06 10:15:45 +0000

    TPL reflect changes.

--- lib/themes/erasmus_30year_anniversary/templates/field-collection/field-collection-item--image-left-row.tpl.php
+++ lib/themes/erasmus_30year_anniversary/templates/field-collection/field-collection-item--image-left-row.tpl.php
@@ -4,17 +4,14 @@
  * Default theme implementation for field collection items.
  */
 ?>
-<?php
-  $class = $element['field_30ya_contentrow_type'][0]['#markup'];
-?>
-<section class="container content-stripe <?php print $class; ?> clearfix"<?php print $attributes; ?>>
- <div class="left"
-    <?php print render($element['field_30ya_contentrow_media']); ?>
+<section class="container content-stripe <?php print $type; ?> clearfix"<?php print $attributes; ?>>
+ <div class="left">
+    <?php print $media; ?>
   </div>
   <div class="right">
     <h2>
-      <?php print render($element['title_field']); ?>
+      <?php print $title_field; ?>
     </h2>
-    <?php print render($element['field_30ya_contentrow_richtext']); ?>
+    <?php print $text; ?>
   </div>
 </section>
