67af705046bb154347dd845ffc8d62970bad5365
michael schuddings <michael.schuddings@one-agency.be>
2017-01-09 07:24:21 +0000

    Adding the correct wrappers to the views tpl again and also naqi theming of heroes tpl.

--- lib/themes/erasmus_30year_anniversary/templates/views/views-view-unformatted--erasmus-30-year-anniversary-heroes--block-30-year-anniversary-heroes-all.tpl.php
+++ lib/themes/erasmus_30year_anniversary/templates/views/views-view-unformatted--erasmus-30-year-anniversary-heroes--block-30-year-anniversary-heroes-all.tpl.php
@@ -1,13 +1,22 @@
-<div class="all-heroes">
-  <a class="open-close-link" href="#" alt="<?php print t('View all heroes'); ?>"><?php print t('View  all heroes'); ?></a>
-  <div class="all-heroes-content">
-    <?php if (!empty($title)): ?>
-      <h3><?php print $title; ?></h3>
-    <?php endif; ?>
-    <?php foreach ($rows as $id => $row): ?>
-      <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-        <?php print $row; ?>
-      </div>
-    <?php endforeach; ?>
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#allHeroes">
+  <?php print t('View all heroes'); ?>
+</button>
+
+<div class="all-heroes modal fade" id="allHeroes" tabindex="-1" role="dialog" aria-labelledby="allHeroes" aria-hidden="true">
+  <h2>
+    <?php print t('All heroes'); ?>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+    </button>
+  </h2>
+  <div class="all-heroes-content modal-dialog" role="document">
+        <?php if (!empty($title)): ?>
+          <h3><?php print $title; ?></h3>
+        <?php endif; ?>
+        <?php foreach ($rows as $id => $row): ?>
+          <article<?php if ($classes_array[$id]) { print ' class="all-heroes-item ' . $classes_array[$id] .'"';  } ?>>
+            <?php print $row; ?>
+          </article>
+        <?php endforeach; ?>
   </div>
 </div>
--- lib/themes/erasmus_30year_anniversary/templates/views/views-view.tpl.php
+++ lib/themes/erasmus_30year_anniversary/templates/views/views-view.tpl.php
@@ -27,66 +27,67 @@
  * @ingroup views_templates
  */
 ?>
-<?php print render($title_prefix); ?>
-<?php if ($title): ?>
-  <?php print $title; ?>
-<?php endif; ?>
-<?php print render($title_suffix); ?>
-
-<?php if ($exposed): ?>
-  <section class="view-filters gradient-stripe gradient-a">
-    <div class="gradient-stripe__content">
-      <?php print $exposed; ?>
-    </div>
-    <div class="background-pattern"></div>
+<div class="<?php print $classes; ?>">
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <?php print $title; ?>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  
+  <?php if ($exposed): ?>
+    <section class="view-filters gradient-stripe gradient-a">
+      <div class="gradient-stripe__content">
+        <?php print $exposed; ?>
+      </div>
+      <div class="background-pattern"></div>
+    </section>
+  <?php endif; ?>
+  
+  <section role="main" class="view-content container">
+    <?php if ($header): ?>
+      <div class="view-header">
+        <?php print $header; ?>
+      </div>
+    <?php endif; ?>
+    
+    <?php if ($attachment_before): ?>
+      <div class="attachment attachment-before">
+        <?php print $attachment_before; ?>
+      </div>
+    <?php endif; ?>
+    
+    <?php if ($rows): ?>
+      <?php print $rows; ?>
+    <?php elseif ($empty): ?>
+      <div class="view-empty">
+        <?php print $empty; ?>
+      </div>
+    <?php endif; ?>
+    
+    <?php if ($pager): ?>
+      <?php print $pager; ?>
+    <?php endif; ?>
+    
+    <?php if ($attachment_after): ?>
+      <div class="attachment attachment-after">
+        <?php print $attachment_after; ?>
+      </div>
+    <?php endif; ?>
+    
+    <?php if ($more): ?>
+      <?php print $more; ?>
+    <?php endif; ?>
+    
+    <?php if ($footer): ?>
+      <div class="view-footer">
+        <?php print $footer; ?>
+      </div>
+    <?php endif; ?>
+    
+    <?php if ($feed_icon): ?>
+      <div class="feed-icon">
+        <?php print $feed_icon; ?>
+      </div>
+    <?php endif; ?>
   </section>
-<?php endif; ?>
-
-<section role="main" class="view-content container">
-  <?php if ($header): ?>
-    <div class="view-header">
-      <?php print $header; ?>
-    </div>
-  <?php endif; ?>
-  
-  <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
-      <?php print $attachment_before; ?>
-    </div>
-  <?php endif; ?>
-  
-  <?php if ($rows): ?>
-    <?php print $rows; ?>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
-  <?php endif; ?>
-  
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
-  
-  <?php if ($attachment_after): ?>
-    <div class="attachment attachment-after">
-      <?php print $attachment_after; ?>
-    </div>
-  <?php endif; ?>
-  
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
-  
-  <?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-  <?php endif; ?>
-  
-  <?php if ($feed_icon): ?>
-    <div class="feed-icon">
-      <?php print $feed_icon; ?>
-    </div>
-  <?php endif; ?>
-</section>
-
+</div>
