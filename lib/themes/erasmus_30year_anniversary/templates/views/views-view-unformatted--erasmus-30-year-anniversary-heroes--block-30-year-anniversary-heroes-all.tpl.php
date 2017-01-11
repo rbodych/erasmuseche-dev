5489bb85eafcf74f22b3438fe3c00232378de375
michael schuddings <michael.schuddings@one-agency.be>
2017-01-11 12:53:35 +0000

    HTML changed medhi + heroes to videos.

--- lib/themes/erasmus_30year_anniversary/templates/views/views-view-unformatted--erasmus-30-year-anniversary-heroes--block-30-year-anniversary-heroes-all.tpl.php
+++ lib/themes/erasmus_30year_anniversary/templates/views/views-view-unformatted--erasmus-30-year-anniversary-heroes--block-30-year-anniversary-heroes-all.tpl.php
@@ -1,10 +1,10 @@
-<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#allHeroes">
-  <?php print t('View all heroes'); ?>
+<button type="button" class="button button--stroke-secondary button--medium" data-toggle="modal" data-target="#allHeroes">
+  <span class="glyphicon glyphicon-th-large" aria-hidden="true"></span> <?php print t('View all heroes'); ?>
 </button>
 
 <div class="all-heroes modal fade" id="allHeroes" tabindex="-1" role="dialog" aria-labelledby="allHeroes" aria-hidden="true">
   <h2>
-    <?php print t('All heroes'); ?>
+    <?php print t('All videos'); ?>
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
       <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
     </button>
@@ -14,7 +14,7 @@
           <h3><?php print $title; ?></h3>
         <?php endif; ?>
         <?php foreach ($rows as $id => $row): ?>
-          <article<?php if ($classes_array[$id]) { print ' class="all-heroes-item ' . $classes_array[$id] .'"';  } ?>>
+          <article class="all-heroes-item">
             <?php print $row; ?>
           </article>
         <?php endforeach; ?>
