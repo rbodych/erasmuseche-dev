b6da9611b24522cbe990a224ffc2a3247fec7bba
michael schuddings <michael.schuddings@one-agency.be>
2017-01-11 11:42:32 +0000

    render feedback form tpl page.

--- lib/themes/erasmus_30year_anniversary/templates/page.tpl.php
+++ lib/themes/erasmus_30year_anniversary/templates/page.tpl.php
@@ -214,6 +214,12 @@ global $base_url;
     <span class="glyphicon glyphicon-chevron-up"></span>
   </a>
 
+<div class="feedbackform">
+  <div class="container">
+    <?php print render($page['feedbackform']); ?>
+  </div>
+</div>
+
   <footer>
     <div class="container">
       <?php print $regions['footer']; ?>
