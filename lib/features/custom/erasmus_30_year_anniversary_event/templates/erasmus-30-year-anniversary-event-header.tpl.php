cfbca5c908a16ef5aad566d4d24f0dbb323e6acc
michael schuddings <michael.schuddings@one-agency.be>
2017-01-11 11:46:17 +0000

    Correct filter on countries.

--- lib/features/custom/erasmus_30_year_anniversary_event/templates/erasmus-30-year-anniversary-event-header.tpl.php
+++ lib/features/custom/erasmus_30_year_anniversary_event/templates/erasmus-30-year-anniversary-event-header.tpl.php
@@ -4,20 +4,20 @@
  * Custom template file for header.
  */
 ?>
-<div class="event-header square-medium">
+<div class="event-header--highlights">
   <?php if (isset($countries)): ?>
     <div class="event-header--country">
-      <?php print format_plural($countries, t('1 country'), t('@count countries')); ?>
+      <?php print format_plural($countries, t('1 country'), t('@count') . '<span>' . t('countries') . '</span>'); ?>
     </div>
   <?php endif; ?>
   <?php if (isset($events)): ?>
     <div class="event-header--events">
-      <?php print format_plural($events, t('1 event'), t('@count events')); ?>
+      <?php print format_plural($events, t('1 event'), t('@count') . '<span>'  .t('events') . '</span>'); ?>
     </div>
   <?php endif; ?>
   <?php if (isset($participants)): ?>
     <div class="event-header--participants">
-      <?php print $participants . ' ' . t('participants'); ?>
+      <?php print $participants . '<span>' . t('participants') . '</span>'; ?>
     </div>
   <?php endif; ?>
 </div>
