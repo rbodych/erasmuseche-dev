dabce0b5ac085d975d755cb18803dee9cba3d879
michael schuddings <michael.schuddings@one-agency.be>
2017-02-10 09:32:05 +0000

    WMS-4185: Video poster.

--- lib/themes/erasmus_30year_anniversary/template.php
+++ lib/themes/erasmus_30year_anniversary/template.php
@@ -100,9 +100,17 @@ function erasmus_30year_anniversary_field_collection_view($variables) {
       $media = _erasmus_30_year_anniversary_page_popup_media_thumb($big_uri, $fc_item['field_30ya_media_thumb'][0]['file']['#item']['uri']);
       $media = _erasmus_30_year_anniversary_page_add_full_screen_link($media);
     }
-    else {
+    if (isset($fc_item['field_30ya_media_poster'])) {
+      $poster = $fc_item['field_30ya_media_poster'];
+      if (!empty($poster)) {
+        $poster = image_style_url('erasmus_30_year_story_huge__720x574', $poster['#items'][0]['file']->uri);
+        $fc_item['field_30ya_contentrow_media'][0]['file']['#files'][0]['poster'] = $poster;
+      }
+    }
+    if (empty($media)) {
       $media = render($fc_item['field_30ya_contentrow_media']);
     }
+
     $element['#children'] = theme('field_collection_item__' . str_replace('-', '_', $fc_item['field_30ya_contentrow_type'][0]['#markup']),
       [
         'text' => render($fc_item['field_30ya_contentrow_richtext']),
@@ -114,4 +122,62 @@ function erasmus_30year_anniversary_field_collection_view($variables) {
     );
   }
   return $element['#children'];
+}
+
+/**
+ * Returns HTML for displaying an HTML5 <video> tag.
+ *
+ * @param array $variables
+ *   An associative array containing:
+ *   - file: Associative array of file data, which must include "uri".
+ *   - controls: Boolean indicating whether or not controls should be displayed.
+ *   - autoplay: Boolean indicating whether or not the video should start
+ *     playing automatically.
+ *   - loop: Boolean indicating whether or not the video should loop.
+ *   - muted: Boolean indicating whether or not the sound should be muted.
+ *   - width: Width, in pixels, of the video player.
+ *   - height: Height, in pixels, of the video player.
+ *
+ * @ingroup themeable
+ */
+function erasmus_30year_anniversary_file_entity_file_video($variables) {
+  $files = $variables['files'];
+  $output = '';
+
+  $video_attributes = array();
+  if ($variables['controls']) {
+    $video_attributes['controls'] = 'controls';
+  }
+  if ($variables['autoplay']) {
+    $video_attributes['autoplay'] = 'autoplay';
+  }
+  if ($variables['loop']) {
+    $video_attributes['loop'] = 'loop';
+  }
+  if ($variables['muted']) {
+    $video_attributes['muted'] = 'muted';
+  }
+  if ($variables['width']) {
+    $video_attributes['width'] = $variables['width'];
+  }
+  if ($variables['height']) {
+    $video_attributes['height'] = $variables['height'];
+  }
+  if (!empty($variables['preload'])) {
+    $video_attributes['preload'] = $variables['preload'];
+  }
+  if (isset($variables['files'][0]['poster']) && !empty($variables['files'][0]['poster'])) {
+    $video_attributes['poster'] = $variables['files'][0]['poster'];
+  }
+
+  $output .= '<video' . drupal_attributes($video_attributes) . '>';
+  foreach ($files as $delta => $file) {
+    $source_attributes = array(
+      'src' => file_create_url($file['uri']),
+      'type' => $file['filemime'],
+    );
+    $output .= '<source' . drupal_attributes($source_attributes) . ' />';
+  }
+  $output .= '</video>';
+  return $output;
 }
