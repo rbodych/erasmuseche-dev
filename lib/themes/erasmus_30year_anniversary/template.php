98230b695bcd7f85d10a21477088b9c4c94414a3
Miguel Martins <zarabatana@users.noreply.github.com>
2017-03-30 09:24:43 +0200

    Update template.php

--- lib/themes/erasmus_30year_anniversary/template.php
+++ lib/themes/erasmus_30year_anniversary/template.php
@@ -189,7 +189,7 @@ function erasmus_30year_anniversary_preprocess_page(&$variables) {
   $spotlight_page = $variables['node'];
   $in_the_spotlight = field_get_items('node', $spotlight_page, 'field_30ya_in_the_spotlight', [LANGUAGE_NONE]);
 
-  if($in_the_spotlight[0]['value'] == 1) {
+  if ($in_the_spotlight[0]['value'] == 1) {
     foreach (field_get_items('node', $spotlight_page, 'field_30ya_in_the_spotlight', [LANGUAGE_NONE]) as $spotlight_page) {
       $variables['monthly_themes'] = views_embed_view('monthly_themes', 'block');
       $variables['sidebar_left'] = $variables['regions']['sidebar_left'] . $variables['regions']['sidebar_right'];
